f100e68391d75146800c6fe0a47c778110bb7839
davincy64 <sergio_ruiz_6@hotmail.com>
2016-03-07 23:32:04 -0600

    se entrega un botón y una etiqueta label que imprime el nombre del alumno alumno sergio cesar ruiz santana
    se entrega un botón y una etiqueta label que imprime el nombre del
    alumno alumno sergio cesar ruiz santana

--- Actividad3/Actividad3/App.cs
+++ Actividad3/Actividad3/App.cs
@@ -6,15 +6,40 @@ namespace Actividad3
 	public class App
 	{
 		public static Page GetMainPage ()
-		{	
+
+		{    
 			return new ContentPage { 
 				Content = new Label {
-					Text = "Hello, Forms!",
+					Text = "TE CLEA EL BOTON Y TE DARA EL NOMBRE DEL ALUMNO!",
 					VerticalOptions = LayoutOptions.CenterAndExpand,
 					HorizontalOptions = LayoutOptions.CenterAndExpand,
+
+
 				},
 			};
 		}
 	}
-}
+}	
+public class ButtonLoggerPage : ContentPage
+{
+	
+	StackLayout loggerLayout = new StackLayout(); 
+	public ButtonLoggerPage()
+	{ 
+		Button button = new Button 
+		{
+			Text = "TECLEA EL BOTÓN!"
+		}; 
+		button.Clicked += OnButtonClicked; 
+		this.Padding = new Thickness(5, Device.OnPlatform(20, 0, 0), 5, 0); 
 
+	}
+	void OnButtonClicked(object sender, EventArgs args)
+	{
+		 
+		loggerLayout.Children.Add(new Label
+			{ Text = "SERGIO CESAR  RUIZ  SANTANA" 
+			}); 
+	}
+}
+	
