f282b69c3beab56fe346f056a312c7bdc0552915
davincy64 <sergio_ruiz_6@hotmail.com>
2016-03-30 16:15:35 -0600

    crear una pantalla de login, del proyecto actividad 5
    crear una pantalla de login, del proyecto actividad 5

---
+++ Actividad5/Actividad5/login.cs
@@ -0,0 +1,45 @@
+﻿using System; 
+using Xamarin.Forms; 
+ 
+namespace Actividad5 
+ { 
+ 	public class Login: ContentPage 
+ 	{ 
+ 		public Login () 
+ 		{ 
+ 			Entry usuario = new Entry { Placeholder = "Usuario" }; 
+ 			Entry clave = new Entry { Placeholder = "Clave", IsPassword = true }; 
+ 
+
+			Button boton = new Button { 
+ 				Text = "Login", 
+ 				TextColor = Color.White, 
+ 				BackgroundColor = Color.FromHex ("77D065")  
+ 			}; 
+ 
+
+ 			boton.Clicked += (sender, e) => { 
+
+ 				if(usuario.Text.Equals("usuario")){ 
+				     Navigation.PushAsync(new Contenido()); 
+			        } 
+			};  
+				
+		StackLayout stackLayout = new StackLayout 
+			{ 
+ 				Spacing = 20,  
+ 				Padding = 50, 
+ 				VerticalOptions = LayoutOptions.Center, 
+ 				Children = 
+				{ 
+ 					usuario, 
+ 					clave, 
+ 					boton 
+ 				} 
+ 				};  
+								 
+        	this.Content = stackLayout; 
+ 			this.Padding = new Thickness (5, Device.OnPlatform (20, 5, 5), 5, 5); 
+ 		} 
+ 	} 
+ } 
