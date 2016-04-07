4a780023698a0e0b9c131de4f0fd6dc095d985b1
davincy64 <sergio_ruiz_6@hotmail.com>
2016-04-06 22:33:59 -0500

    Abre el proyecto Actividad 8 e integra ImágeneAbre el proyecto Actividad 8 e integra Imágenes a las nuevas páginas del  carusel que se creó. s a
    Abre el proyecto Actividad 8 e integra Imágenes a las nuevas páginas del
    carusel que se creó.

---
+++ Actividad8/Actividad8/pagina 1.cs
@@ -0,0 +1,48 @@
+﻿using System; 
+using Xamarin.Forms; 
+
+namespace Actividad8 
+{ 
+	public class Pagina1: ContentPage 
+	{ 
+		public Pagina1 () 
+		{ 
+			Label texto = new Label { 
+				Text = "Página 1", 
+				TextColor = Color.Blue
+			}; 
+
+			Button boton = new Button 
+			{ 
+				Text = " Navegar en la página2" 
+			}; 
+
+			boton.Clicked += (sender, e) => { 
+				this.Navigation.PushAsync(new Pagina2()); 
+			}; 
+
+			StackLayout stackLayout = new StackLayout 
+			{ 
+				Children = 
+				{ 
+					texto, 
+					boton 
+
+
+				}
+				}; 
+			var uri = new Uri("https://www.google.com.mx/search?q=fotos+unam&biw=1366&bih=599&tbm=isch&imgil=vLae-78WzBBQSM%253A%253Be4i4D3-rnD4hEM%253Bhttp%25253A%25252F%25252Fmexicoenergetico.com.mx%25252Funam-y-cfe-planean-desarrollar-en-conjunto-proyectos-en-materia-energetica%25252F&source=iu&pf=m&fir=vLae-78WzBBQSM%253A%252Ce4i4D3-rnD4hEM%252C_&usg=__QANMYfQidT1dwKrJZrdGx8nGOLc%3D"); 
+			var uri2 = new Uri("https://www.google.com.mx/search?q=fotos+unam&biw=1366&bih=599&tbm=isch&imgil=4DmW4TqzrA3hZM%253A%253B9IWs90_fg3eV2M%253Bhttp%25253A%25252F%25252Fwww.vanguardia.com.mx%25252Farticulo%25252Ffuera-de-peligro-mexicanas-que-estudian-en-bruselas-unam&source=iu&pf=m&fir=4DmW4TqzrA3hZM%253A%252C9IWs90_fg3eV2M%252C_&usg=__jyhcRet-qWOtzbthLYOiacsjDoI%3D"); 
+
+			var image = new Image { Aspect = Aspect.AspectFit }; 
+
+			image.Source = ImageSource.FromUri(uri);
+			image.Source = ImageSource.FromUri(uri2);
+
+
+
+			this.Content = stackLayout; 
+			this.Padding = new Thickness (5, Device.OnPlatform (20, 5, 5), 5, 5); 
+		} 
+	} 
+} 
---
+++ Actividad8/Actividad8/pagina 2.cs
@@ -0,0 +1,37 @@
+﻿using System; 
+using Xamarin.Forms; 
+
+namespace Actividad8 
+{ 
+	public class Pagina2: ContentPage 
+	{ 
+		public Pagina2 () 
+		{ 
+			Label texto = new Label { 
+				Text = "Página 2", 
+				TextColor = Color.Blue 
+			}; 
+
+			StackLayout stackLayout = new StackLayout 
+			{ 
+				Children = 
+				{ 
+					texto  
+
+				} 
+				}; 
+			
+			var uri3 = new Uri("https://www.google.com.mx/imgres?imgurl=http://www.educacionyculturaaz.com/wp-content/uploads/2013/07/Escudo-UNAM-A.jpg&imgrefurl=http://www.educacionyculturaaz.com/educacion/convoca-unam-a-formar-coleccion-de-libros-digitales&h=640&w=1024&tbnid=obD65tKAlOIcNM:&docid=tyDdHI65hkhjzM&ei=QYwEV-_7OOOwjgSB0qSADQ&tbm=isch&ved=0ahUKEwivoKSpkvnLAhVjmIMKHQEpCdAQMwhGKB0wHQ"); 
+			var uri4 = new Uri("https://www.google.com.mx/imgres?imgurl=https://pbs.twimg.com/profile_images/616985983863525376/zCFU4CM0.jpg&imgrefurl=https://twitter.com/fundacion_unam&h=512&w=512&tbnid=4oGM3A_K2qm6PM:&docid=65mQO7tRdEm1uM&ei=kYwEV76fIIWDjgTs7qSIBg&tbm=isch&ved=0ahUKEwj-q57PkvnLAhWFgYMKHWw3CWE4ZBAzCC8oKjAq"); 
+
+			var image = new Image { Aspect = Aspect.AspectFit }; 
+
+			image.Source = ImageSource.FromUri(uri3);
+			image.Source = ImageSource.FromUri(uri4);
+
+
+			this.Content = stackLayout; 
+			this.Padding = new Thickness (5, Device.OnPlatform (20, 5, 5), 5, 5); 
+		} 
+	} 
+} 
